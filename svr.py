4075c258da1450080b061e7c57aa02e84bcc5601
Kevin <stradtkt22@gmail.com>
2019-05-20 20:33:16 -0400

    Added the template and the dataset

--- svr.py
+++ svr.py
@@ -4,4 +4,22 @@ Created on Mon May 20 20:11:42 2019
 
 @author: Kevin
 """
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+from sklearn.svm import SVR
+regressor = SVR(kernal = 'rbf')
+
+y_pred = regressor.predict(6.5)
+
+plt.scatter(X, y, color='red')
+plt.plot(X, regressor.predict(X), color='blue')
+plt.title('Truth or Bluff (SVR)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
